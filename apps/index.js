6e0d445e6122d7569bc0be30a75f563b0cf37958
rafid-al-mahmud <rafidalmahmudper09@gmail.com>
2022-04-20 09:54:35 +0600

    v0.0.1

---
+++ apps/index.js
@@ -0,0 +1,9 @@
+const BUILD_AWESOME_SOFTWARE_APPLICATION = document.querySelector("._ba-app_");
+const app = BUILD_AWESOME_SOFTWARE_APPLICATION;
+app.render=(x)=>{
+    document.querySelector("._app_")
+}
+app.render({
+    name:"Rafid",
+    baid:""
+},``)
